3cb822e7c038d4b8716be0e2650ac3476cdba85f
lizhi <949168418@qq.com>
2022-01-15 09:08:19 +0800

    提交项目

---
+++ src/main/java/cn/es/controller/CityRestController.java
@@ -0,0 +1,19 @@
+package cn.es.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/es")
+public class CityRestController {
+
+    @GetMapping("/query")
+    @ResponseBody
+    public String query() {
+        return "dd";
+    }
+
+
+}
